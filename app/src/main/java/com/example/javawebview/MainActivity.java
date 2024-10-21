e21eec91c235661968f439ac079f876f97a66c88
tonyreed <treed@dealerinspire.com>
2024-10-21 15:29:34 -0500

    add basic webview example to be iterated upon

---
+++ app/src/main/java/com/example/javawebview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.javawebview;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://www.google.com");
+    }
+}
